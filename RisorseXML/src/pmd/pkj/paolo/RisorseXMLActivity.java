41f51fbe94ecdc45c81cf58c3de6a874184e99ce
paoletto <paoletto@paoletto-VirtualBox.(none)>
2012-01-20 17:56:58 +0100

    ex2 15/12/2011

---
+++ RisorseXML/src/pmd/pkj/paolo/RisorseXMLActivity.java
@@ -0,0 +1,13 @@
+package pmd.pkj.paolo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RisorseXMLActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
